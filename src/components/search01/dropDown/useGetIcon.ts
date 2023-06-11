5d6396685acd3d809269c91580916955e998238f
erickdevup <erickdevup@gmail.com>
2023-06-11 20:34:04 +0200

    component ui search01

---
+++ src/components/search01/dropDown/useGetIcon.ts
@@ -0,0 +1,8 @@
+import React from "react";
+
+type Props = { show: boolean };
+
+//https://api.iconify.design/search?query=nextjs
+//https://api.iconify.design/teenyicons/nextjs-outline.svg
+
+export default function useGetIcon({ show }: Props) {}
